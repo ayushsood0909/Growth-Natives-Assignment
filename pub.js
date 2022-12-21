7ecc8333fd64647bc92d3f7b0bb0d7263ae00472
Ayush <soodayush72@gmail.com>
2022-12-21 21:18:04 +0530

    initial commit

---
+++ pub.js
@@ -0,0 +1,12 @@
+const mqtt=require("mqtt");
+var client=mqtt.connect('mqtt://127.0.0.1:1883');
+
+client.on("connect",function(){
+    setInterval(function(){
+        var random=Math.random()*50;
+        console.log(random);
+        if(random<30){
+            client.publish('Pradeep Kumar','Simple MQTT using HiveMQ:'+random.toString()+'.')
+        }
+    }),30000;
+});
---
+++ sub.js
@@ -0,0 +1,27 @@
+const mqtt = require("mqtt");
+
+var client = mqtt.connect("mqtt://127.0.0.1:1883");
+
+
+
+client.on("connect",function()
+
+{
+
+    setInterval(function(){
+
+        var random = Math.random()*50;
+
+        console.log(random);
+
+        if(random<30)
+
+        {
+
+            client.publish("Shirish","temperature value: "+random.toString());
+
+        }
+
+    }),30000;
+
+});
